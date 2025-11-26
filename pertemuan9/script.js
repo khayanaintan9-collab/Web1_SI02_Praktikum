af146eea56888111ac738ea278b7535d2b09c656
Khayana Tantri <khayanaintan9@gmail.com>
2025-11-27 00:14:22 +0700

    Add files via upload

---
+++ pertemuan9/script.js
@@ -0,0 +1,7 @@
+function tambahBorder() {
+  document.getElementById("box").style.border = "3px solid red";
+}
+
+function hapusBorder()  {
+  document.getElementById("box").style.border= "none" ;
+}
